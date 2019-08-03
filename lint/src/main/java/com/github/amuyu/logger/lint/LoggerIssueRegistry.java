cce729f8475dc35ad90dc2a6c6522b7367423960
amuyu <hwajoong.song@gmail.com>
2019-08-03 16:28:59 +0900

    Add getIssues method

--- lint/src/main/java/com/github/amuyu/logger/lint/LoggerIssueRegistry.java
+++ lint/src/main/java/com/github/amuyu/logger/lint/LoggerIssueRegistry.java
@@ -5,6 +5,7 @@ import com.android.tools.lint.detector.api.Issue;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class LoggerIssueRegistry extends IssueRegistry {
@@ -12,6 +13,6 @@ public class LoggerIssueRegistry extends IssueRegistry {
     @NotNull
     @Override
     public List<Issue> getIssues() {
-        return null;
+        return Arrays.asList(WrongUsageDetector.getIssues());
     }
 }
--- lint/src/main/java/com/github/amuyu/logger/lint/WrongUsageDetector.java
+++ lint/src/main/java/com/github/amuyu/logger/lint/WrongUsageDetector.java
@@ -1,7 +1,11 @@
 package com.github.amuyu.logger.lint;
 
 import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Issue;
 
 public class WrongUsageDetector extends Detector implements Detector.UastScanner {
 
+    static Issue[] getIssues() {
+        return new Issue[]{ };
+    }
 }
